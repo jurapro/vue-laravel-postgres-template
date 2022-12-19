e07c8819189714777d472bc53c09725c13a2c12d
jurapro <jurapro@yandex.ru>
2022-12-19 18:43:46 +0700

    example template ddd

--- backend/database/factories/Domain/Shared/UserFactory.php
+++ backend/database/factories/Domain/Shared/UserFactory.php
@@ -5,9 +5,6 @@ namespace database\factories\Domain\Shared;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Shared\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
